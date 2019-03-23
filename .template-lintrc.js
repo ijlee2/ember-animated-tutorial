b51241ed2b5e0bac413670a6cd933ebe723e299c
ijlee2 <ijlee2@users.noreply.github.com>
2019-03-23 10:11:40 -0500

    Installed Ember addons

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+    extends: 'recommended',
+    rules: {
+        'attribute-indentation': {
+            'indentation': 4,
+        },
+        'block-indentation': 4,
+        'inline-link-to': true,
+        'link-href-attributes': true,
+        'link-rel-noopener': 'strict',
+        'no-bare-strings': false,
+        'no-inline-styles': false, // go against the recommended
+        'no-trailing-dot-in-path-expression': true,
+        'no-trailing-spaces': true,
+    },
 };
