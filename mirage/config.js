5bb3dd992528d0605ca81200df03d47db8f164fa
ijlee2 <ijlee2@users.noreply.github.com>
2019-03-25 17:26:04 -0500

    Made Students page responsive

--- mirage/config.js
+++ mirage/config.js
@@ -138,7 +138,7 @@ export default function() {
 
         students.forEach(student => {
             // Create a score between 0 and 99
-            let relativeScore = Math.ceil(99 * (student.metadata.score - minScore) / (maxScore - minScore));
+            let relativeScore = Math.floor(99 * (student.metadata.score - minScore) / (maxScore - minScore));
             const numDigits = relativeScore.toString().length;
 
             relativeScore = `${'0'.repeat(2 - numDigits)}${relativeScore}`;
