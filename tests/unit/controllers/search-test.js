f2dcea8d3e5a78a781de9fc252d7587767ab8d66
ijlee2 <ijlee2@users.noreply.github.com>
2019-03-25 10:58:34 -0500

    Added unit tests

--- tests/unit/controllers/search-test.js
+++ tests/unit/controllers/search-test.js
@@ -1,12 +1,203 @@
+import EmberObject from '@ember/object';
 import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
 module('Unit | Controller | search', function(hooks) {
     setupTest(hooks);
 
-    // Replace this with your real tests.
-    test('it exists', function(assert) {
+    hooks.beforeEach(function(assert) {
         let controller = this.owner.lookup('controller:search');
-        assert.ok(controller);
+
+        controller.setProperties({
+            query: '',
+            selectedSkills: [],
+            remainingSkills: [
+                EmberObject.create({
+                    id: '1',
+                    name: 'ActionScript',
+                    type: 'miscellaneous',
+                    synonyms: [],
+                    isSelected: false,
+                }),
+
+                EmberObject.create({
+                    id: '2',
+                    name: 'Ember',
+                    type: 'frontend',
+                    synonyms: ['Ember.js'],
+                    isSelected: false,
+                }),
+
+                EmberObject.create({
+                    id: '3',
+                    name: 'Java',
+                    type: 'backend',
+                    synonyms: [],
+                    isSelected: false,
+                }),
+
+                EmberObject.create({
+                    id: '4',
+                    name: 'JavaScript',
+                    type: 'frontend',
+                    synonyms: ['ES6', 'ES7', 'ES8', 'ES2017', 'ES2018', 'ES2019'],
+                    isSelected: false,
+                }),
+            ],
+        });
+
+        controller.set('filteredRemainingSkills', controller.remainingSkills);
+
+        assert.strictEqual(
+            controller.selectedSkills.length,
+            0,
+            'We see 0 selected skills.'
+        );
+
+        assert.strictEqual(
+            controller.remainingSkills.length,
+            4,
+            'We see 4 remaining skills.'
+        );
+
+        assert.strictEqual(
+            controller.filteredRemainingSkills.length,
+            4,
+            'We see 4 filtered remaining skills.'
+        );
+    });
+
+
+    test('We can search the remaining skills', function(assert) {
+        let controller = this.owner.lookup('controller:search');
+
+        // Do a search for the query 'java'
+        controller.set('query', 'java');
+        controller.send('searchSkills');
+
+        assert.deepEqual(
+            controller.filteredRemainingSkills.mapBy('name'),
+            ['Java', 'JavaScript'],
+            'We see the correct remaining skills.'
+        );
+
+        // Do a search for the query 'script'
+        controller.set('query', 'script');
+        controller.send('searchSkills');
+
+        assert.deepEqual(
+            controller.filteredRemainingSkills.mapBy('name'),
+            ['ActionScript', 'JavaScript'],
+            'We see the correct remaining skills.'
+        );
+
+        // Do a search for the query '123'
+        controller.set('query', '123');
+        controller.send('searchSkills');
+
+        assert.deepEqual(
+            controller.filteredRemainingSkills.mapBy('name'),
+            [],
+            'We see the correct remaining skills.'
+        );
+
+        // Do a search for the empty query
+        controller.set('query', '');
+        controller.send('searchSkills');
+
+        assert.deepEqual(
+            controller.filteredRemainingSkills.mapBy('name'),
+            ['ActionScript', 'Ember', 'Java', 'JavaScript'],
+            'We see the correct remaining skills.'
+        );
+    });
+
+
+    test('We can select and unselect skills', function(assert) {
+        let controller = this.owner.lookup('controller:search');
+
+        // Select a skill
+        controller.send('selectSkill', controller.remainingSkills.objectAt(1));
+
+        assert.deepEqual(
+            controller.selectedSkills.mapBy('name'),
+            ['Ember'],
+            'We see the correct selected skills.'
+        );
+
+        assert.deepEqual(
+            controller.remainingSkills.mapBy('name'),
+            ['ActionScript', 'Java', 'JavaScript'],
+            'We see the correct remaining skills.'
+        );
+
+        assert.deepEqual(
+            controller.filteredRemainingSkills.mapBy('name'),
+            ['ActionScript', 'Java', 'JavaScript'],
+            'We see the correct filtered remaining skills.'
+        );
+
+        // Select another skill
+        controller.send('selectSkill', controller.remainingSkills.objectAt(2));
+
+        assert.deepEqual(
+            controller.selectedSkills.mapBy('name'),
+            ['Ember', 'JavaScript'],
+            'We see the correct selected skills.'
+        );
+
+        assert.deepEqual(
+            controller.remainingSkills.mapBy('name'),
+            ['ActionScript', 'Java'],
+            'We see the correct remaining skills.'
+        );
+
+        assert.deepEqual(
+            controller.filteredRemainingSkills.mapBy('name'),
+            ['ActionScript', 'Java'],
+            'We see the correct filtered remaining skills.'
+        );
+
+        // Unselect a skill
+        controller.send('unSelectSkill', controller.selectedSkills.objectAt(0));
+
+        assert.deepEqual(
+            controller.selectedSkills.mapBy('name'),
+            ['JavaScript'],
+            'We see the correct selected skills.'
+        );
+
+        assert.deepEqual(
+            controller.remainingSkills.mapBy('name'),
+            ['ActionScript', 'Java', 'Ember'],
+            'We see the correct remaining skills.'
+        );
+
+        assert.deepEqual(
+            controller.filteredRemainingSkills.mapBy('name'),
+            ['ActionScript', 'Java', 'Ember'],
+            'We see the correct filtered remaining skills.'
+        );
+
+        // Unselect another skill
+        controller.send('unSelectSkill', controller.selectedSkills.objectAt(0));
+
+        assert.deepEqual(
+            controller.selectedSkills.mapBy('name'),
+            [],
+            'We see the correct selected skills.'
+        );
+
+        assert.deepEqual(
+            controller.remainingSkills.mapBy('name'),
+            ['ActionScript', 'Java', 'Ember', 'JavaScript'],
+            'We see the correct remaining skills.'
+        );
+
+        assert.deepEqual(
+            controller.filteredRemainingSkills.mapBy('name'),
+            ['ActionScript', 'Java', 'Ember', 'JavaScript'],
+            'We see the correct filtered remaining skills.'
+        );
     });
 });
--- tests/unit/models/student-test.js
+++ tests/unit/models/student-test.js
@@ -4,10 +4,16 @@ import { module, test } from 'qunit';
 module('Unit | Model | student', function(hooks) {
     setupTest(hooks);
 
-    // Replace this with your real tests.
-    test('it exists', function(assert) {
+    test('We see the correct computed properties', function(assert) {
         let store = this.owner.lookup('service:store');
-        let model = store.createRecord('student', {});
-        assert.ok(model);
+        let model = store.createRecord('student', {
+            firstName: 'Jane',
+            lastName: 'Smith',
+            email: 'jane.smith@example.com',
+            phone: '(123) 456-7890',
+        });
+
+        assert.strictEqual(model.fullName, 'Jane Smith', 'We see the correct full name.');
+        assert.strictEqual(model.internationalPhone, '+1-123-456-7890', 'We see the correct international phone.');
     });
 });
