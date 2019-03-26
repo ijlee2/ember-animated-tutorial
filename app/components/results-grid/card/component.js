e3303789c911b0997cdbaa3a99078bc48c627a71
ijlee2 <ijlee2@users.noreply.github.com>
2019-03-25 21:14:25 -0500

    Working on writing semantic HTML

--- app/components/results-grid/card/component.js
+++ app/components/results-grid/card/component.js
@@ -4,6 +4,7 @@ import { readOnly } from '@ember/object/computed';
 export default Component.extend({
     'data-test-card': readOnly('student.fullName'),
     classNames: ['results-grid__card'],
+    tagName: 'article',
 
     actions: {
         loadDefaultImage() {
--- app/components/student-details/component.js
+++ app/components/student-details/component.js
@@ -4,6 +4,7 @@ import { alias } from '@ember/object/computed';
 export default Component.extend({
     'data-test-details': true,
     classNames: ['student-details'],
+    tagName: 'article',
 
     // For simplicity, we assume that each student has only one resume
     resume: alias('student.resumes.firstObject'),
