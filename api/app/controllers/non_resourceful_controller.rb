ff6ebac83ed9eb26147929921d91159993f90f0d
ijlee2 <ijlee2@users.noreply.github.com>
2019-03-28 06:55:56 -0500

    Can find skills

--- api/app/controllers/non_resourceful_controller.rb
+++ api/app/controllers/non_resourceful_controller.rb
@@ -1,8 +1,29 @@
 class NonResourcefulController < ApplicationController
-    # GET /search (queryParams: skillIds)
+    # GET /search (queryParams: sid)
     def search
+        desiredSkillIds = params[:sid].split(',');
+
+        # Find the skills that the students must meet
+        desiredSkills = Skill.find(desiredSkillIds)
+            .reduce([]) { |accumulator, skill|
+                accumulator << skill.name.downcase
+
+                if (!skill.synonyms.empty?)
+                    skill.synonyms.each do |synonym|
+                        accumulator << synonym.downcase
+                    end
+                end
+
+                accumulator
+            }
+
+        # Find the students that meet the skills
+        minScore = 0
+        maxScore = 1
+
         render json: {
-            foo: 'bar'
+            foo: desiredSkillIds,
+            bar: desiredSkills
         }
     end
 end
