035b5a290552af83c7fa2430cd23224977d8b329
ijlee2 <ijlee2@users.noreply.github.com>
2019-04-02 14:39:12 -0500

    Starter for 1c. findAll bug

--- app/routes/students/student.js
+++ app/routes/students/student.js
@@ -4,7 +4,7 @@ export default Route.extend({
     model(params) {
         console.log(`Load student with id ${params.id}`);
 
-        return this.store.findRecord('student', params.id, {
+        return this.store.loadRecord('student', params.id, {
             include: 'resumes,resumes.degrees,resumes.experiences,resumes.skills',
         });
     },
